ce262527d6bd221ee3a81931eb110b5e38472161
Kirill Kutovuy <114417403+Kirill99k@users.noreply.github.com>
2023-03-05 19:30:12 +0200

    done gallery-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = crateImageCardsMarkup(galleryItems);
+
+function crateImageCardsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  scrollZoom: false,
+});
